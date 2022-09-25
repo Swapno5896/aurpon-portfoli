d24ae18bbbb11d3bc9b119eccd0e8cff6fc86c0f
swaono5896 <swapnom73@gmail.com>
2022-09-25 17:16:39 +0600

    single card is redy

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import './BlogCard.css'
 const BlogCard = () => {
     return (
         <div className='col-md-4 col-sm-6 '>
-            <div class="card"  >
+            <div class="card blog-card rounded-4"  >
                 <img src={require('../../images/invictus.png')} class="card-img-top" alt="..." />
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                    <h5 class="card-title">How To Get 7 In IELTS</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" class="btn btn-primary blog-button">Read More</a>
                 </div>
             </div>
         </div>
