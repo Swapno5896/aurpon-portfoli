b23933319234a6cf28bf94d3c47b6e29b61f5b11
swaono5896 <swapnom73@gmail.com>
2022-10-09 17:25:25 +0600

    topp banner added

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import './TopBanner.css'
 const TopBanner = () => {
     return (
-        <div>
-
+        <div className='container pb-5 mb-5'>
+            <div className="row">
+                <div className="col-md-6 text-start d-flex justify-content-center align-items-center">
+                    <div>
+                        <h1>Hello I'm
+                            <br />
+                            <span className='topbanner-name'>Aurpon Chowdhury</span>
+                        </h1>
+                        <p className='topbanner-study'>International Student At Alberta</p>
+                        <p className='topbanner-detal'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem corporis iste placeat in unde nisi molestiae dicta sapiente et dignissimos repudiandae, rerum quaerat! Optio saepe laudantium quam nesciunt esse totam?</p>
+                    </div>
+                </div>
+                <div className="col-md-6 topbanner-image-container">
+                    <img src={require("../../images/dada-2.jpg")} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,7 @@ import SmallVideo from '../../components/SmallVideo/SmallVideo';
 import Couter from '../../components/Couter/Couter';
 import Review from '../../components/Revidw/Review';
 import ContactMessage from '../../components/ContactMessage/ContactMessage';
+import TopBanner from '../../components/TopBanner/TopBanner';
 const Home = () => {
     const data = [
         { a: 3 },
@@ -15,6 +16,7 @@ const Home = () => {
     return (
         <div id='home'>
             <div className="container  mt-5 pt-5">
+                <TopBanner></TopBanner>
                 <div className="row text-start">
 
                     {data.map(d => <BlogCard></BlogCard>)}
